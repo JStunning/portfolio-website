1d9868440e5050fa3913fc58163701ee5dc32d3a
Jack Dunning <JackStunning9001@gmail.com>
2021-01-26 14:35:05 -0800

    Add Skills section and change title

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import './App.css';
 import { Button, AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
 import { MenuIcon } from '@material-ui/icons/Menu'; 
+import Skills from './components/Skills'
 
 
 function App() {
   return (
     <div className="App">
-      {/* <AppBar position="static">
+      <AppBar>
         <Toolbar>
-          <IconButton edge="start" color="black" aria-label="menu">
+          {/* <IconButton color="black" aria-label="menu">
             <MenuIcon />
-          </IconButton>
+          </IconButton> */}
           <Typography variant="h6">
             News
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
-      </AppBar> */}
+      </AppBar>
       <header className="App-header">
         <Button variant="contained">Default</Button>
       </header>
+      <Skills />
     </div>
   );
 }
---
+++ src/components/Skills.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+
+//skills section
+export class Skills extends Component {
+  render(){
+    return (
+      <div className="Skills">
+        <h2>Skills</h2>
+      </div>
+    )
+  }  
+}
+
+export default Skills
