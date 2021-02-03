01e18a30b05d693e86326d222fc7b326f3dfd0ba
Jack Dunning <JackStunning9001@gmail.com>
2021-02-03 14:17:18 -0800

    Update App and Css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import './App.css';
-import { Button, AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
-import { MenuIcon } from '@material-ui/icons/Menu'; 
-import Skills from './components/Skills'
-
+import Navbar from './components/Navbar';
+import Skills from './components/Skills';
+import Footer from './components/Footer';
+import { Button } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
-      <AppBar>
-        <Toolbar>
-          {/* <IconButton color="black" aria-label="menu">
-            <MenuIcon />
-          </IconButton> */}
-          <Typography variant="h6">
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-      <header className="App-header">
-        <Button variant="contained">Default</Button>
-      </header>
-      <Skills />
+      <Navbar />
+      <div className="App-showcase">
+          <Button variant="contained" >Default</Button>
+        </div>
+      <div>
+        <Skills />
+      </div>
+      <Footer />
     </div>
   );
 }
