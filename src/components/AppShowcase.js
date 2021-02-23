00b7b8f5b08f03cc4417ec88433fffd61094574d
Jack Dunning <JackStunning9001@gmail.com>
2021-02-23 00:40:43 -0800

    Add styling for ipad pro

--- src/components/AppShowcase.js
+++ src/components/AppShowcase.js
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import { Typography } from '@material-ui/core';
 import { FiChevronsDown } from 'react-icons/fi';
 
