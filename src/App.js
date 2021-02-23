78cb676983bd8233a83fd8709a12a699130c8487
Jack Dunning <JackStunning9001@gmail.com>
2021-02-23 01:22:46 -0800

    Remove unused code

--- src/App.js
+++ src/App.js
@@ -2,21 +2,16 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
-import ContactPage from './pages/ContactPage'
-import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
 
 const App = () => {
   return (
-    <Router>
+    <>
       <Navbar />
       <div className="App">
-        <Route exact path="/portfolio-website/" component={HomePage} />
-        <Route exact path='/portfolio-website/contact'>
-          <ContactPage />
-        </Route>
+        <HomePage />
       </div>
       <Footer />
-    </Router>
+    </>
   );
 }
 
